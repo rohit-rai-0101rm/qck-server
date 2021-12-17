87d3398de3bba305a7f70aa37663f7478485c3fe
rohit rai <rohitrai.dev@gmail.com>
2021-12-17 16:47:55 +0530

    order and cart controllers added

---
+++ controllers/orderController.js
@@ -0,0 +1,76 @@
+const Order = require("../models/Order");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+exports.newOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = new Order(req.body);
+  const savedOrder = await order.save;
+
+  res.status(201).json({
+    success: true,
+    savedCart,
+  });
+});
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const updatedCart = await Cart.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    { new: true }
+  );
+
+  res.status(201).json({
+    success: true,
+    updatedCart,
+  });
+});
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  await Order.findByIdAndDelete(req.params.id);
+
+  res.status(201).json({
+    success: true,
+    message: "order deleted succesfully",
+  });
+});
+exports.getUserOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ userId: req.params.userId });
+ 
+  res.status(201).json({
+    success: true,
+    orders,
+  });
+});
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find();
+  
+    res.status(201).json({
+      success: true,
+      carts,
+    });
+  });
+  exports.getOrderStats = catchAsyncErrors(async (req, res, next) => {
+    const date=new Date();
+    const lastMonth=new Date(date.setMonth(date.getMonth()-1))
+    const previousMonth=new Date(new Date().setMonth(lastMonth.getMonth()-1))
+
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales:"$amount"
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: $sales },
+        },
+      },
+    ]);
+      res.status(200).json({
+        success: true,
+        income,
+      });
+    });
+  
