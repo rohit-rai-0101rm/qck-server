751f5502495a0725ae87c0087e5be06c6f2396c8
rohit rai <rohitrai.dev@gmail.com>
2021-12-17 13:39:27 +0530

    update product route added

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,3 +11,55 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
       newProduct,
     });
   });
+  exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+  
+    if (!product) {
+      return next(new ErrorHander("Product not found", 404));
+    }
+  
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  });
+  
+  // Update Product -- Admin
+  
+  exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+  
+    if (!product) {
+      return next(new ErrorHander("Product not found", 404));
+    }
+  
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+  
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  });
+  
+  // Delete Product
+  
+  exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+  
+    if (!product) {
+      return next(new ErrorHander("Product not found", 404));
+    }
+  
+  
+    await product.remove();
+  
+    res.status(200).json({
+      success: true,
+      message: "Product Delete Successfully",
+    });
+  });
+  
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createProduct } = require("../controllers/productController");
+const { createProduct, getProductDetails, updateProduct, deleteProduct } = require("../controllers/productController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
@@ -7,3 +7,9 @@ router
   .route("/admin/product/new")
   .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
   module.exports = router;
+  router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  //.delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+//router.route("/product/:id").get(getProductDetails);
