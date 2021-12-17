cd59c33d3d8b5adf4fd094d9b38fce2935caccb4
rohit rai <rohitrai.dev@gmail.com>
2021-12-17 21:45:45 +0530

    product controller modified

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -13,4 +13,5 @@ router
   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
   router.route("/products").get(getAllProducts);
 
-router.route("/product/:id").get(getProductDetails);
+router.route("/product/:id").get(getProductDetails)
+module.exports = router;
